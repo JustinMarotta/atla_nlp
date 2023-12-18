55b142dbb09515d981f924fd0f2ed0d562b99e58
Justin Marotta <109969478+JustinMarotta@users.noreply.github.com>
2023-12-18 16:14:22 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='properly implement sentiment analysis using a transformer architecture #perform tokenization and preprocessing from scratch #finetune existing transformer architecture on processed transcripts from Avatar: The Last Airbender #showcase how each character's sentiment/emotion changes over the course of the show (Aang, Katara, Sokka, Toph, Zuko, Iroh) #showcase how to use the model to predict sentiment on unseen data #gain insights into the show's storytelling from a psychological perspective',
+    author='Justin Marotta',
+    license='MIT',
+)
